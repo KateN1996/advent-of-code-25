0bbe3a2a90ac907411998a758356d6bc0aa832c1
Kate Nixon <98198608+KateN1996@users.noreply.github.com>
2025-12-01 19:04:45 -0700

    golang's int division can go fuck itself

--- day_1/decode-password.go
+++ day_1/decode-password.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	decodePassword("input.txt")
+	decodePassword2("input.txt")
 }
 
 func decodePassword(fileName string) int {
@@ -54,7 +54,6 @@ func decodePassword2(fileName string) int {
 	}
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
-
 	numOfZeros := 0
 	curVal := 50
 
@@ -62,29 +61,36 @@ func decodePassword2(fileName string) int {
 		line := scanner.Text()
 		direction := line[0]
 		num, err := strconv.Atoi(line[1:])
-
 		if err != nil {
 			panic(err)
 		}
-		fmt.Printf("num is %d, dir is %d\n", num, direction)
 
+		oldVal := curVal
 		if direction == 'L' {
-			numOfZeros += (curVal+1)/100 - (curVal-num+1)/100
 			curVal -= num
 		} else {
-			numOfZeros += (curVal+num)/100 - curVal/100
 			curVal += num
 		}
-		fmt.Printf("curval is %d\n", curVal)
 
-		//numOfZeros += int(math.Abs(float64(curVal)) / 100)
-		curVal = ((curVal % 100) + 100) % 100
-
-		if curVal == 0 {
-			numOfZeros += 1
-			fmt.Printf("curval is 0; zeroes are now %d\n\n", numOfZeros)
+		if direction == 'L' {
+			numOfZeros += floorDiv(oldVal-1, 100) - floorDiv(curVal-1, 100)
+		} else {
+			numOfZeros += floorDiv(curVal, 100) - floorDiv(oldVal, 100)
 		}
+		fmt.Printf("zeroes are now %d\n\n", numOfZeros)
+
+		curVal = ((curVal % 100) + 100) % 100
 	}
+
 	fmt.Printf("\n\nthis is the password: %d\n", numOfZeros)
 	return numOfZeros
+}
+
+func floorDiv(a, b int) int {
+	q := a / b
+	r := a % b
+	if (r != 0) && ((r < 0) != (b < 0)) {
+		q--
+	}
+	return q
 }
